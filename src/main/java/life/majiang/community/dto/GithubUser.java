2a6e5e1f817bbe43d2daf197202c9d114afb4d24
za664971838 <664971838@qq.com>
2021-01-31 20:46:25 +0800

    extract github config from code to properties

---
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -0,0 +1,43 @@
+package life.majiang.community.dto;
+
+/**
+ * Created by codedrinker on 2019/4/24.
+ */
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+    private String avatarUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
